4950a1312534b7d6d1d35555a5a5fab3bbdc89d6
Christoph Mahlo <xcm.dos12@gmail.com>
2023-10-17 22:15:54 +0200

    Initial commit

---
+++ shockwavetest/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace shockwave_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            //this.axShockwaveFlash1 = new AxShockwaveFlashObjects.AxShockwaveFlash();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            //((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axShockwaveFlash1
+            // 
+            /*this.axShockwaveFlash1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axShockwaveFlash1.Enabled = true;
+            this.axShockwaveFlash1.Location = new System.Drawing.Point(0, 0);
+            this.axShockwaveFlash1.Name = "axShockwaveFlash1";
+            this.axShockwaveFlash1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axShockwaveFlash1.OcxState")));
+            this.axShockwaveFlash1.Size = new System.Drawing.Size(1273, 507);
+            this.axShockwaveFlash1.TabIndex = 0;*/
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Random";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(124, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 27);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Download";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(292, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(398, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 27);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(492, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(176, 27);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Diashow";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(674, 14);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(780, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ms";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(812, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(89, 27);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Start";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(907, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(89, 27);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Stop";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1002, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(89, 27);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Back";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(1097, 12);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(89, 27);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "Forward";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1273, 507);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            //this.Controls.Add(this.axShockwaveFlash1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            //((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        //private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ shockwavetest/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace shockwave_test
+{
+    public partial class Form1 : Form
+    {
+        string num;
+        int i = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            string fol = Application.StartupPath;
+            Directory.SetCurrentDirectory(fol);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            num = random.Next(7600).ToString();
+            this.Text = num;
+            //axShockwaveFlash1.Movie = "http://z0r.de/L/z0r-de_" + num + ".swf";
+            //axShockwaveFlash1.Play();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            num = random.Next(7600).ToString();
+            this.Text = num;
+            //axShockwaveFlash1.Movie = "http://z0r.de/L/z0r-de_" + num + ".swf";
+            //axShockwaveFlash1.Play();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile("http://z0r.de/L/z0r-de_" + num + ".swf", "z0r-de_" + num + ".swf");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Text = textBox1.Text;
+            num = textBox1.Text;
+            //axShockwaveFlash1.Movie = "http://z0r.de/L/z0r-de_" + textBox1.Text + ".swf";
+            //axShockwaveFlash1.Play();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "")
+            {
+                i = 1;
+                timer1.Start();
+                timer1.Interval = Convert.ToInt32(textBox2.Text);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            num = i.ToString();
+            this.Text = num;
+            //axShockwaveFlash1.Movie = "http://z0r.de/L/z0r-de_" + i + ".swf";
+            //axShockwaveFlash1.Play();
+            if (i >= 7911)
+            {
+                timer1.Stop();
+            }
+            i = i + 1;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer1.Interval = Convert.ToInt32(textBox2.Text);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            i = Convert.ToInt32(num);
+            i = i - 1;
+            num = i.ToString();
+            this.Text = num;
+            //axShockwaveFlash1.Movie = "http://z0r.de/L/z0r-de_" + i + ".swf";
+            //axShockwaveFlash1.Play();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            i = Convert.ToInt32(num);
+            i = i + 1;
+            num = i.ToString();
+            this.Text = num;
+            //axShockwaveFlash1.Movie = "http://z0r.de/L/z0r-de_" + i + ".swf";
+            //axShockwaveFlash1.Play();
+        }
+    }
+}
